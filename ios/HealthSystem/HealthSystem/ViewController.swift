2e2403e4305f040e809f1a2fbfc6d056ec08b99f
Elina Karimova <elinakarimova@Elinas-MacBook-Pro.local>
2025-10-15 17:10:10 +0200

    Add SwiftUIKit iOS project HealthApp

---
+++ ios/HealthSystem/HealthSystem/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  HealthSystem
+//
+//  Created by Elina Karimova on 15/10/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        view.backgroundColor = .red
+    }
+
+
+}
+
